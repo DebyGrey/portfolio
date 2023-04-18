43cb220bafc75775ec43970abee31a863c89d4ef
DebyGrey <demifashoro@gmail.com>
2023-04-18 15:52:13 +0100

    Setup for mobile menu

---
+++ index.js
@@ -0,0 +1,14 @@
+function initMenu() {
+  const hamburger = document.querySelector('#hamburger');
+  // const closeBtn = document.querySelector('#close-hamburger');
+  const mobileMenu = document.querySelector('.mobile-menu');
+  if (hamburger) {
+    hamburger.addEventListener('click', () => {
+      mobileMenu.classList.toggle('show-menu');
+    });
+  }
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  initMenu();
+});
